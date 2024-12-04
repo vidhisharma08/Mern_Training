6b3cbc0d4a217cf4c8a5eeeadde5f55771fbd389
vidhisharma08 <vidhisharma2908@gmail.com>
2024-12-04 09:21:35 +0530

    Added Project

---
+++ NodeProject/fruits/kiwi.js
@@ -0,0 +1,5 @@
+let k={
+    name : "kiwi",
+    
+}
+module.export=k
--- NodeProject/index.js
+++ NodeProject/index.js
@@ -1,2 +1,18 @@
-const fruits = require("./fruits")
-console.log(fruits.mango);
+// const fruits = require("./fruits")
+// console.log(fruits.mango);
+
+//--figlet
+
+// var figlet = require("figlet");
+
+// figlet("Aayush", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+let f =require('./f1')
+f(7,8);
